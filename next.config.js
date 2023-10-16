7b6b997821fa133f3f04aad6651fa1b9bcf40fe1
Vishalreddy90 <vishalreddy9039@gmail.com>
2023-10-16 19:24:29 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-   images: { unoptimized: true, } 
-}
-
-  
-  module.exports = nextConfig
+   output: 'export'
+ }
+ 
+ module.exports = nextConfig
